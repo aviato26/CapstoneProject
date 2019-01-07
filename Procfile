cf8bfd508d9bf5d037810ef0fe3d4d5b067aa60d
shaun deshler <shaun@shauns-iMac.local>
2019-01-06 17:40:21 -0800

    moved react app to client folder and server to the root directory

--- Procfile
+++
@@ -1,2 +0,0 @@
-
-web: node react-scripts start
--- server.js
+++ server.js
@@ -35,9 +35,9 @@ var sess = {
 }
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('./build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
